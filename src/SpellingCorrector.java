04250a35314a39812f8252721c9b99662cc7ae8b
kenlo <kenlo.dinhvan@gmail.com>
2017-10-08 16:45:53 -0400

    initial sync

--- src/SpellingCorrector.java
+++ src/SpellingCorrector.java
@@ -1,5 +1,210 @@
+import java.io.*;
+import java.util.*;
+
+
 class SpellingCorrector {
     public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+
+        //read every line of vocab.txt
+        ArrayList <String> dictionary = readDictionary("testcase1");
+
+        //read sentence.txt
+        ArrayList <String> sentence = readSentence("testcase1");
+
+        //
+        int desiredLevenshteinDistance = readLevenshteinDistance("testcase1");
+
+        //flag each word not in the dictionary
+
+        //to store misspelled word en their correction
+        Map<String, ArrayList<String>> misspelledWordsCorrectionsMap = new LinkedHashMap<String, ArrayList<String>>();
+
+
+        for (int i = 0; i < sentence.size(); i++){
+
+            ArrayList<String> correctionsList = new ArrayList<String>();
+
+            String searchedWord = sentence.get(i);
+
+            if (dictionary.contains(searchedWord)){
+//                System.out.println("the Word '"+searchedWord + "' is in the Dictionary.");
+            }
+
+            else {
+                String misspelledWord = searchedWord;
+//                System.out.println("the Word '"+searchedWord + "' is NOT in the Dictionary.");
+
+                LevenshteinDistance levenshteinDistanceCalculator = new LevenshteinDistance();
+
+                for (int j=0; j<dictionary.size(); j++){
+
+                    int lvshtnD = levenshteinDistanceCalculator.computeLevenshteinDistance(misspelledWord, dictionary.get(j));
+
+                    if (lvshtnD == desiredLevenshteinDistance){
+                        //add a possible correction
+                        correctionsList.add(dictionary.get(j));
+//                      System.out.println("'"+misspelledWord + "' is close to '"+dictionary.get(j)+"'");
+                    }
+
+                }
+                misspelledWordsCorrectionsMap.put(misspelledWord,correctionsList);
+            }
+        }
+
+        //print
+        printResults(misspelledWordsCorrectionsMap);
+
+        //write to text file
+        writeResultsToFile(misspelledWordsCorrectionsMap);
+
+
+
+
+
+
+
+        //output to MisspelledWords.txt
+        //each line:
+        // misspelledWord1: correction1, correction2, correction3
+        //0 if none
+
+
+        //USING BK Trees
+
     }
+
+    public static ArrayList <String> readDictionary(String testCaseStr){
+        //read every line of vocab.txt, max 400 000 words of 30 characters
+        ArrayList <String> dictionaryArrayList = new ArrayList<String>();
+
+        try {
+
+            File dictionaryFile = new File("testCases/"+ testCaseStr + "/vocab.txt");
+            FileReader dictionaryFileReader = new FileReader(dictionaryFile);
+            BufferedReader dictionaryBufferedReader = new BufferedReader(dictionaryFileReader);
+            StringBuffer dictionary = new StringBuffer();
+            String definedWordPerLine;
+            while ((definedWordPerLine = dictionaryBufferedReader.readLine()) != null) {
+//                dictionary.append(definedWordPerLine);
+//                dictionary.append("\n");
+                  dictionaryArrayList.add(definedWordPerLine);
+            }
+            dictionaryFileReader.close();
+
+
+//            System.out.println("Contents of dictionary file:");
+//            System.out.println(Arrays.toString(dictionaryArrayList.toArray()));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dictionaryArrayList;
+    }
+
+    public static ArrayList <String> readSentence(String testCaseStr){
+        ArrayList <String> sentenceArrayList = new ArrayList<String>();
+        try {
+
+            File sentenceFile = new File("testCases/"+ testCaseStr + "/sentence.txt");
+            FileReader sentenceFileReader = new FileReader(sentenceFile);
+            BufferedReader sentenceBufferedReader = new BufferedReader(sentenceFileReader);
+//            StringBuffer sentenceStringBuffer = new StringBuffer();
+            String sentenceLine;
+
+            while ((sentenceLine = sentenceBufferedReader.readLine()) != null) {
+                // break the line into individual words
+                // the String array contains each word of the current line
+                sentenceArrayList = new ArrayList<String>(Arrays.asList(sentenceLine.split(" +")));
+
+//                sentenceStringBuffer.append(sentenceLine);
+//                sentenceStringBuffer.append("\n");
+            }
+
+            sentenceFileReader.close();
+
+//            System.out.println("Contents of sentence file:");
+//            System.out.println(Arrays.toString(sentenceArrayList.toArray()));
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return sentenceArrayList;
+    }
+
+    public static int readLevenshteinDistance (String testCaseStr){
+        int levenshteinDistance=0;
+        try {
+
+            File distanceFile = new File("testCases/"+ testCaseStr + "/MaxDistance.txt");
+            FileReader distanceFileReader = new FileReader(distanceFile);
+            BufferedReader distanceBufferedReader = new BufferedReader(distanceFileReader);
+//            StringBuffer distanceStringBuffer = new StringBuffer();
+            String distanceLine;
+
+            while ((distanceLine = distanceBufferedReader.readLine()) != null) {
+                levenshteinDistance = Integer.parseInt(distanceLine);
+
+            }
+
+            distanceFileReader.close();
+
+//            System.out.print("Contents of MaxDistance file: ");
+//            System.out.println(levenshteinDistance);
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return levenshteinDistance;
+    }
+
+    /**
+     * A Simple Program That Prints An Array In Java using Own logic.
+     */
+
+        private static String arrayListToString(ArrayList<String> anArray) {
+            String strArrayList ="";
+            for (int i = 0; i < anArray.size(); i++) {
+                if (i > 0) {
+//                  add a comma space before the next correction
+                    strArrayList += ", ";
+                }
+
+//              add the correction
+                strArrayList += anArray.get(i);
+
+
+            }
+            return strArrayList;
+        }
+
+    public static void writeResultsToFile(Map<String, ArrayList<String>> resultMap) {
+        PrintWriter writer = null;
+        try {
+            writer = new PrintWriter("MisspelledWords.txt", "UTF-8");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry : resultMap.entrySet()) {
+            String mispelledWord = entry.getKey();
+            ArrayList<String> correctionList = entry.getValue();
+            writer.println(mispelledWord + ": " + arrayListToString(correctionList));
+        }
+        writer.close();
+    }
+    public static void printResults (Map<String, ArrayList<String>> resultMap) {
+        for (Map.Entry<String, ArrayList<String>> entry : resultMap.entrySet()) {
+            String mispelledWord = entry.getKey();
+            ArrayList<String> correctionList = entry.getValue();
+            System.out.println(mispelledWord + ": " + arrayListToString(correctionList));
+        }
+    }
+
+
 }
